ae6475dc5f49a052c88facf3a3e539a2ccfb57a1
teodoragolubovic99 <tg20180415@student.fon.bg.ac.rs>
2022-05-17 23:48:04 +0200

    Migracije za nove tabele

---
+++ database/migrations/2022_05_17_214456_create_proizvods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProizvodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proizvods', function (Blueprint $table) {
+            $table->id();
+            $table->string('naziv');
+            $table->string('velicina');
+            $table->double('cena');
+            $table->foreignId('brend_id')->constrained('brends');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proizvods');
+    }
+}
